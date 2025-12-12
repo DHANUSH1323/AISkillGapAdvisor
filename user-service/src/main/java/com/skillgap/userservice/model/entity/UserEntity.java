fc1804f56dc62ae7b76046df941442d053ef63a9
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2025-12-12 15:51:57 -0500

    added lombok to avoid boiler codes

--- user-service/src/main/java/com/skillgap/userservice/model/entity/UserEntity.java
+++ user-service/src/main/java/com/skillgap/userservice/model/entity/UserEntity.java
@@ -3,24 +3,20 @@ package com.skillgap.userservice.model.entity;
 import com.skillgap.userservice.model.UserStatus;
 import java.util.UUID;
 import java.time.Instant;
-
 import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Entity
 @Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserEntity {
 
     @Id
