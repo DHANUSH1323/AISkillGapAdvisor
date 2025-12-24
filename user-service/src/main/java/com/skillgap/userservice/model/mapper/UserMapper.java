29a648e7f37666667f229afb909e28d5870da7e5
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2025-12-23 17:31:53 -0800

    added mapper conversion

--- user-service/src/main/java/com/skillgap/userservice/model/mapper/UserMapper.java
+++ user-service/src/main/java/com/skillgap/userservice/model/mapper/UserMapper.java
@@ -1,5 +1,31 @@
 package com.skillgap.userservice.model.mapper;
 
-public class UserMapper {
-    
+import com.skillgap.userservice.model.dto.response.UserResponseDto;
+import com.skillgap.userservice.model.entity.UserEntity;
+
+import java.util.Objects;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper extends BaseMapper<UserEntity, UserResponseDto>{
+
+    @Override
+    public UserEntity convertToEntity(UserResponseDto dto, Object... args){
+        UserEntity entity = new UserEntity();
+        if(Objects.nonNull(dto)){
+            BeanUtils.copyProperties(dto, entity);
+        }
+        return entity;
+    }
+
+    @Override
+    public UserResponseDto convertToDto(UserEntity entity, Object... args){
+        UserResponseDto dto = new UserResponseDto();
+        if(Objects.nonNull(entity)){
+            BeanUtils.copyProperties(entity, dto);
+        }
+        return dto;
+    }
 }
